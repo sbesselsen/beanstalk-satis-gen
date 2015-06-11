71007bedc2093ea80ae58aa38576cad5ff474f44
Anton Timmermans <me@atimmer.com>
2015-06-11 12:24:25 +0200

    Tag version 0.2.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## 0.2.1
+
+### Fixed
+- Fixed a bug where parsed_to could be set to `null` in some cases
+
 ## 0.2.0
 
 ### Added
--- index.php
+++ index.php
@@ -5,7 +5,7 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
     require_once(__DIR__ . '/vendor/autoload.php');
 }
 
-$application = new \Symfony\Component\Console\Application('Beanstalk Satis Generator', '0.2.0');
+$application = new \Symfony\Component\Console\Application('Beanstalk Satis Generator', '0.2.1');
 $application->addCommands(array(
     new \BeanstalkSatisGen\Commands\GenerateCommand(),
     new \BeanstalkSatisGen\Commands\UpdateCommand(),
